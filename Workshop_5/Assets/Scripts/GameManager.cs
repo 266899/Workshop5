7a0d444a7bbbbed60b403728bec1bc3f5741f9c1
2 <266899@via.dk>
2019-04-28 18:56:00 +0200

    Startup
    Added planets, scripts to move

---
+++ Workshop_5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] planets;
+    public GameObject[] placeholders;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnRandomly();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnRandomly()
+    {
+        //Fix
+        // Random planet spawned around
+        for (int i = 0; i < placeholders.Length; i++)
+        {
+            int randomPlanet = (int)Math.Floor(Random.Range(0f, planets.Length));
+            Debug.Log(randomPlanet);
+            planets[randomPlanet].transform.position = placeholders[i].transform.position;
+            planets[randomPlanet].SetActive(true);
+        }
+    }
+}
